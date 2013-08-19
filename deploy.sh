7fd1119401e2d1f07498a9c006aec1b8a364ded6
James Alexander <james@jamesralexander.com>
2013-08-18 22:17:02 -0400

    - Adding deployment script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+siteFolder=/home/yanigisawa/jamesralexander.com/webchat
+
+cp chat.py $siteFolder/chat.py
+cp index.html $siteFolder/index.html
+cp javascript.py $siteFolder/javascript.py
+cp img/*.png $siteFolder/img
+cp chatClient.js $siteFolder/chatClient.js
