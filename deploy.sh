5269b2feec7cae587ccfe1a72a48c33e8417bc33
James Alexander <james@jamesralexander.com>
2013-09-29 14:02:28 -0700

    Adding new file to deploy script. - Changing javascript.py header to server python location

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 siteFolder=/home/yanigisawa/jamesralexander.com/webchat
 
 cp chat.py $siteFolder/chat.py
+cp chatModels.py $siteFolder/chatModels.py
 cp index.html $siteFolder/index.html
 cp javascript.py $siteFolder/javascript.py
 cp img/*.png $siteFolder/img
--- javascript.py
+++ javascript.py
@@ -1,11 +1,9 @@
-#!/Users/yanigisawa/.virtualenvs/pywebchat/bin/python
-
+#!/usr/bin/python
 
 import cgi;
 import cgitb; cgitb.enable();
 
 javascriptFile = "chatClient.js"
-
 
 import os.path, time
 print("Content-type: text/javascript")
