af7b16da0b965ffb3075d5fe1beaa0316bbeaccc
James Alexander <james@jamesralexander.com>
2013-11-24 21:58:49 -0500

    - Adding Procfile for heroku startup - Adding port environment variable specification for heroku

--- chat.py
+++ chat.py
@@ -164,7 +164,7 @@ def poll():
 def readMessages():
     return getMessages()
 
-    
 #debug(True)
-run(server='paste', reloader=True)
+#run(server='paste', reloader=True)
+run(server='paste', host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
